caf1958e813a33af092b2a34fa5131c802839581
andrasfelfoldi <andras.felfoldi@gmail.com>
2019-04-16 17:29:45 +0200

    deleted some unnecessary stuff

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,16 +1,11 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
 import MainScreen from '../Containers/MainScreen'
-import EditNoteScreen from '../Containers/EditNoteScreen'
-import NotesScreen from '../Containers/NotesScreen'
-import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator({
   MainScreen: { screen: MainScreen },
-  EditNoteScreen: { screen: EditNoteScreen },
-  NotesScreen: { screen: NotesScreen },
 }, {
   // Default config for all screens
   headerMode: 'none',
