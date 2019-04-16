e3aeec9d196238b212d3d01f9c96816e3cf8d685
andrasfelfoldi <andras.felfoldi@gmail.com>
2019-04-16 17:04:49 +0200

    note taking now works with schedule and eliminate

--- App/Components/RightCard.js
+++ App/Components/RightCard.js
@@ -5,10 +5,12 @@ import styles from './Styles/RightCardStyle'
 import { Colors, Metrics } from '../Themes'
 import ToDoEntry from './ToDoEntry';
 import TitleBar from './TitleBar';
+import NoteActions from "../Redux/NoteRedux";
+import { connect } from "react-redux";
 // import { View } from 'react-native-animatable';
 
 
-export default class RightCard extends Component {
+class RightCard extends Component {
   // // Prop type warnings
   // static propTypes = {
   //   someProperty: PropTypes.object,
@@ -23,34 +25,25 @@ export default class RightCard extends Component {
   primaryColor = this.props.title === 'Schedule' ? Colors.blue : Colors.red;
   lightColor = this.props.title === 'Schedule' ? Colors.lightBlue : Colors.lightRed;
 
+  markSchedule = (index) => {
+    this.props.markSchedule(index);
+  }
+
+  markEliminate = (index) => {
+    this.props.markEliminate(index);
+  }
+
+  markNote = this.props.title === 'Schedule' ? this.markSchedule : this.markEliminate;
+
   render () {
     return (
       <View style={{...styles.rightCard,
         backgroundColor: this.lightColor }}>
         <ScrollView style={styles.scrollView} contentContainerStyle={{ flexGrow: 1 }}>
 
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
-          <ToDoEntry color={this.primaryColor}/>
+        {this.props.notes.map((note, index) => 
+            <ToDoEntry key={index} completed={note.isMarkedDone}
+              text={note.text} onPress={() => this.markNote(index)} color={this.primaryColor}/>)}
 
         </ScrollView>
 
@@ -60,3 +53,18 @@ export default class RightCard extends Component {
     )
   }
 }
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    notes: ownProps.title === 'Schedule' ? state.notes.schedules : state.notes.eliminates,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    markSchedule: (index) => dispatch(NoteActions.markSchedule(index)),
+    markEliminate: (index) => dispatch(NoteActions.markEliminate(index)),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(RightCard);
